25562947fe445fd9a91b535c578477b9b04ac2d3
8nau <8nau@fresnoU.org>
2023-02-16 10:28:46 -0800

    initial.commit update

--- input2.js
+++ input2.js
@@ -1,8 +1,2 @@
-//5
-const readline = require("readline-sync");
-
-let cost = Number(readline.question("Enter Price"));
-let tax = cost * 0.08;
-let newcost = cost+ tax;
-newcost = newcost.toFixed(2);
-console.log(newcost);
+//1
+L
