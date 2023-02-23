c4775b1bfbeaaac4ab85feadef1fff05d60f64e0
8nau <8nau@fresnoU.org>
2023-02-23 10:28:33 -0800

    initial.commit forloops  progress

---
+++ forloops.js
@@ -0,0 +1,24 @@
+
+// for(let i = 0; i<=40; i){
+//     console.log(i**2);
+// }
+
+//3
+//for(let i = 0; i<=40; i++) {
+  //  console.log(i ** 2);
+//}
+
+//3
+//for(let i = 0; i<=30; i++) {
+ //console.log(i ** 3);
+//}
+
+//4
+//for(let i = 0; i<=30; i++) {
+   // console.log(i ** 3);
+//}
+
+//5
+for(let i = 350; i<=10; i++) {
+    console.log(i ** 10);
+}
